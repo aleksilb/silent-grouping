8d5f2380eaa5eede36e3054cb97184ae5d7f7b94
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2021-10-06 00:10:16 +0300

    Create a new grouping

--- client/src/Components/NewGrouping.js
+++ client/src/Components/NewGrouping.js
@@ -1,9 +1,46 @@
-import {Button} from "@mui/material";
+import {Button, TextField} from "@mui/material";
+import {useState} from "react";
+const API = 'http://localhost:5000';
 
 function NewGrouping(props) {
+    const [name, setName] = useState(null);
+    const [description, setDescription] = useState(null);
+
+    function createGrouping() {
+        const data = {
+            name: name,
+            description: description
+        }
+
+        fetch(API + '/grouping', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(result => {
+            console.log(result)
+        });
+
+        props.finishFunction();
+    }
 
     return <div>
-        <Button variant="contained" onClick={props.finishFunction}>Start new grouping</Button>
+        <TextField
+            id="name-field"
+            label="Name"
+            variant="outlined"
+            onChange={evt => setName(evt.target.value)}
+        />
+        <TextField
+            id="description-field"
+            label="Description"
+            variant="outlined"
+            rows={4}
+            multiline
+            onChange={evt => setDescription(evt.target.value)}
+        />
+        <Button variant="contained" onClick={createGrouping}>Start new grouping</Button>
     </div>
 }
 
