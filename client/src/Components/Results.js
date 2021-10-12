dfbe7abee23e0efcb6df8483e63211e471d46248
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2021-10-13 00:13:22 +0300

    Wait for results

--- client/src/Components/Results.js
+++ client/src/Components/Results.js
@@ -1,24 +1,56 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import * as Server from '../scripts/server';
 import List from "@mui/material/List";
 import {ListItem} from "@mui/material";
 
 function Results(props) {
     const [groups, setGroups] = useState([]);
+    const [waiting, setWaiting] = useState(true);
+
+    const pollWait = function () {
+        timerRef.current = setTimeout(checkStage, 500);
+    };
+
+    const pollWaitRef = useRef(pollWait);
+    const timerRef = useRef(0);
 
     useEffect(() => {
+        pollWaitRef.current();
+
+        return () => {
+            clearTimeout(timerRef.current);
+        }
+    }, []);
+
+    function checkStage() {
+        Server.getGrouping(props.groupingId)
+            .then(grouping => {
+                if(grouping.stage === 'Done') {
+                    setWaiting(false);
+                    clearTimeout(timerRef.current);
+                    getGroups();
+                } else {
+                    pollWait();
+                }
+            })
+    }
+
+    function getGroups() {
         Server.getGroups(props.groupingId).then(groups => {
             setGroups(groups);
         });
-    }, [props.groupingId]);
+    }
 
     return <div>
         <h1>Results</h1>
-        {groups.map(group =>
-            <List>
-                {group.map(item =>
-                    <ListItem>{item}</ListItem>)}
-            </List>)}
+        {waiting ?
+            <div>waiting...</div>
+            :
+            groups.map(group =>
+                <List>
+                    {group.map((item, index) =>
+                        <ListItem key={index}>{item}</ListItem>)}
+                </List>)}
     </div>
 }
 
