991a64c812d7ed9236f123ed79388eb72a06d82d
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2021-11-04 21:56:02 +0200

    Added plus to item field

--- client/src/Components/TermList.js
+++ client/src/Components/TermList.js
@@ -1,10 +1,11 @@
 import List from '@mui/material/List'
-import {Button, Grid, IconButton, ListItem, TextField} from "@mui/material";
+import {Button, Grid, IconButton, InputAdornment, ListItem, TextField} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import {useState} from "react";
 import * as Server from "../scripts/server";
 import Title from "./Title";
 import Box from "@mui/material/Box";
+import AddIcon from '@mui/icons-material/Add';
 
 function TermList(props) {
     const [newTerm, setNewTerm] = useState('');
@@ -44,7 +45,6 @@ function TermList(props) {
         <Grid container spacing={2} sx={{my: 0}}>
             <Grid item xs={3}/>
             <Grid item xs={6}>
-
                 <Box sx={{my: 3}}>
                     <TextField
                         id="new-term"
@@ -53,6 +53,14 @@ function TermList(props) {
                         onChange={evt => updateNewTerm(evt)}
                         onKeyPress={evt => handleInputKeypress(evt)}
                         value={newTerm}
+                        InputProps={{endAdornment:
+                            <InputAdornment position="end">
+                                <IconButton
+                                    onClick={addNewTerm}
+                                    edge="end">
+                                    <AddIcon/>
+                                </IconButton>
+                            </InputAdornment>}}
                     />
                     <Box sx={{flexBasis: "100%"}}/>
                     <Button variant="contained" onClick={sendTerms} sx={{my: 3, ml: 3}}>Finish</Button>
